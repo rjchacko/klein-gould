45455bf2ef87e75d3d42215f5398be98ee712654
erg.dominguez <erg.dominguez@9a1f5bf3-a548-0410-9e96-a52f46714620>
2008-05-05 02:29:41 +0000

    holy crap.  it may actually be working now.

--- JavaSims/src/rachele/ising/testCode/apps/jamaTestApp.java
+++ JavaSims/src/rachele/ising/testCode/apps/jamaTestApp.java
@@ -6,24 +6,33 @@ import rachele.util.MathTools;
 public class jamaTestApp{
 
     public static void main(String[] argv){   
-    int L = 1012;
+    int L = 3;
    	double [][] M = new double[L][L];
 	
-   	
-   	for(int x = 0; x < L; x++){
-   		for(int y = 0; y <= x; y++){
-   			M[x][y] = M[y][x] = x*y;
-   		}
-   	}
-
-//   	System.out.println("Here comes the matrix:");
+   	M[0][0] = 1;
+   	M[0][1] = 2;
+   	M[0][2] = 0;
+   	M[1][0] = 0;
+   	M[1][1] = 3;
+   	M[1][2] = 0;
+   	M[2][0] = 2;
+   	M[2][1] = -4;
+   	M[2][2] = 2;
 //   	for(int x = 0; x < L; x++){
-//   		for(int y = 0; y < L; y++){
-//   			System.out.print("" + M[x][y] + " ");
+//   		for(int y = 0; y <= x; y++){
+//   			M[x][y] = M[y][x] = x*y;
 //   		}
-//   		System.out.println(" ");
 //   	}
-   	
+
+   	System.out.println("Here comes the matrix:");
+   	for(int x = 0; x < L; x++){
+   		for(int y = 0; y < L; y++){
+   			System.out.print(" " + M[x][y] + " ");
+   		}
+   		System.out.println(" ");
+   	}
+	System.out.println(" ");
+		
 	Matrix A = new Matrix(M);	
 	EigenvalueDecomposition Eig = A.eig();
 	
@@ -31,22 +40,41 @@ public class jamaTestApp{
 	Matrix V = Eig.getV();
 	double [][] VV = V.transpose().getArray();//take transpose to get eigenvectors as row
 
-//	System.out.println(VV[0][0] + " " + VV[0][1]+ " " + VV[0][2]);
-//	System.out.println(VV[1][0] + " " + VV[1][1]+ " " + VV[1][2]);
-//	System.out.println(VV[2][0] + " " + VV[2][1]+ " " + VV[2][2]);
-
+	System.out.println("Here comes V");
+		for(int x = 0; x < L; x++){
+     		for(int y = 0; y < L; y++){
+     			System.out.print(" " + VV[x][y] + " ");
+     		}
+     		System.out.println(" ");
+     	}
+   		System.out.println(" ");
+   		
 	VV = MathTools.normalizeRows(VV);
-//	System.out.println(VV[0][0] + " " + VV[0][1]+ " " + VV[0][2]);
-//	System.out.println(VV[1][0] + " " + VV[1][1]+ " " + VV[1][2]);
-//	System.out.println(VV[2][0] + " " + VV[2][1]+ " " + VV[2][2]);
+	System.out.println("Here comes V normalized");
+	for(int x = 0; x < L; x++){
+ 		for(int y = 0; y < L; y++){
+ 			System.out.print(" " + VV[x][y] + " ");
+ 		}
+ 		System.out.println(" ");
+ 	}
+		System.out.println(" ");
 		
 	double normalizedCheck = MathTools.dot(VV[0],VV[0]);
 	System.out.println("dot prod = " + normalizedCheck);
 
-	double orthogCheck = MathTools.dot(VV[0],VV[11]);
+	double orthogCheck = MathTools.dot(VV[0],VV[1]);
 	System.out.println("dot prod = " + orthogCheck);
 	
 	Matrix D = Eig.getD();
+	double [][] DD = D.transpose().getArray();//take transpose to get eigenvectors as row
+	System.out.println("Here comes D");
+	for(int x = 0; x < L; x++){
+ 		for(int y = 0; y < L; y++){
+ 			System.out.print(" " + DD[x][y] + " ");
+ 		}
+ 		System.out.println(" ");
+ 	}
+		System.out.println(" ");
 	double[] eigenvalue0 = D.transpose().getArray()[0];
 	double[] eigenvalue1 = D.transpose().getArray()[1];
 	double[] eigenvalue2 = D.transpose().getArray()[2];
@@ -55,6 +83,28 @@ public class jamaTestApp{
 	System.out.println(eigenvalue2[0] + " " + eigenvalue2[1]+ " " + eigenvalue2[2]);
 
 	double[] e = Eig.getRealEigenvalues();
-	System.out.println(e[0] + " " + e[1] + " " + e[2]);
+	for(int i = 0; i <L; i++){
+		System.out.println("ev " + i + " = " + e[i]);
+	}
+    
+    //SVD
+	SingularValueDecomposition Svd = A.svd();
+	double [] sv = new double [L];
+	sv = Svd.getSingularValues();
+
+		for(int i = 0; i <L; i++){
+			System.out.println("sv " + i + " = " + sv[i]);
+   		}
+
+		
+		double sum = 0;
+		int testInt=1;
+	    for(int i = 0; i < L; i++){
+	    		sum += M[testInt][i]*VV[testInt][i];
+	    }     
+	    double lambda = sum/VV[testInt][testInt];
+	    System.out.println("ev = " + e[testInt] + " lambda = " + lambda);
     }
+
+    
 }
