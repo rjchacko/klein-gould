32371b4af7626698f276b6bcfbb72f81979de526
erg.dominguez <erg.dominguez@9a1f5bf3-a548-0410-9e96-a52f46714620>
2008-05-16 22:45:52 +0000

    working on mod dynamics

--- JavaSims/src/rachele/ising/dim2/IsingField2D.java
+++ JavaSims/src/rachele/ising/dim2/IsingField2D.java
@@ -308,7 +308,7 @@ public class IsingField2D extends AbstractIsing2D{
 				dF_dPhi = -phi_bar[i]+T*(phi[i]+pow(phi[i],3)) - H;	
 				Lambda[i] = 1;
 			}else{
-				dF_dPhi = -phi_bar[i]+T* scikit.numerics.Math2.atanh(phi[i])- H;
+				dF_dPhi = -phi_bar[i]+T* scikit.numerics.Math2.atanh(phi[i])- H;//
 				//dF_dPhi = -phi_bar[i]+T*(-log(1.0-phi[i])+log(1.0+phi[i]))/2.0 - H;
 				if(theory == "HalfStep"){
 					Lambda[i] = 1;
