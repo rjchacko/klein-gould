c131e3b86dcca40a03906f0b6bbd728858256d40
erg.dominguez <erg.dominguez@9a1f5bf3-a548-0410-9e96-a52f46714620>
2008-05-18 19:03:43 +0000

    some commenting in test linear theory app

---
+++ JavaSims/src/rachele/ising/dim2/apps/TestLinearOptApp.java
@@ -0,0 +1,25 @@
+package rachele.ising.dim2.apps;
+
+import scikit.jobs.Simulation;
+
+public class TestLinearOptApp extends Simulation{
+
+	@Override
+	public void animate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
