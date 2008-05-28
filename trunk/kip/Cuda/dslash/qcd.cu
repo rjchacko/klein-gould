7f7465c403a3fc1573d3f7e35e404c4eee6412f3
kbarros <kbarros@9a1f5bf3-a548-0410-9e96-a52f46714620>
2008-05-28 23:50:16 +0000

    dslash closer to working.

--- trunk/kip/Cuda/dslash/qcd.cu
+++ trunk/kip/Cuda/dslash/qcd.cu
@@ -10,89 +10,68 @@
 
 // includes, kernels
 #include "qcd.h"
-#include "qcd_kernel.cu"
 
 
-extern "C"
-void computeGold(float* reference, float* vdata, float *mdata, const unsigned int len);
+texture<float4, 1, cudaReadModeElementType> gaugeTex;
+texture<float4, 1, cudaReadModeElementType> spinorTex;
 
-void printVector(int i, float *v) {
-    printf("%d : {(%f %f) (%f %f) (%f %f)}\n", i, v[0*L], v[1*L], v[2*L], v[3*L], v[4*L], v[5*L]);
+
+__global__ void
+testKernel(float4* g_out) {
+    #include "qcd_core.cu"
 }
 
 
-void
-runTest( int argc, char** argv)  {
+void runTest( int argc, char** argv)  {
     CUT_DEVICE_INIT(argc, argv);
     
     unsigned int timer = 0;
     cutCreateTimer(&timer);
     
     int shared_bytes = SHARED_BYTES;
-
     printf("Spinors: %d\n", L);
-    printf("Global kb: %f\n", L*(GAUGE_BYTES+SPINOR_BYTES)/1024.);
+    printf("Global kb: %f\n", L*(PACKED_GAUGE_BYTES+SPINOR_BYTES)/1024.);
     printf("Shared kb: %fkB\n", shared_bytes/1024.);
     
-    // allocate host memory
-    float* h_vdata = (float*) malloc(L*SPINOR_BYTES);
-    float* h_mdata = (float*) malloc(L*GAUGE_BYTES);
-    float* h_odata = (float*) malloc(L*SPINOR_BYTES);
-    int *h_index   =   (int*) malloc(8*L*sizeof(int));
-    float* reference = (float*) malloc(L*SPINOR_BYTES);
-    
-    // initialize indices
-    for (int d = 0; d < 8; d++) {
-    
-        for (int i = 0; i < L; i++) {
-            h_index[d*L+i] = i;
-        }
-    
-    }
-    
-    // initialize gauge field
-    for (int m = 0; m < 3; m++) {
-        for (int n = 0; n < 3; n++) {
-            for(int i = 0; i < L; i++) {
-                h_mdata[L*(2*(3*m + n) + 0) + i] = 1;
-                h_mdata[L*(2*(3*m + n) + 1) + i] = -1;
-            }
-        }
-    }
-    // initialize spinor field
-    for (int s = 0; s < 4; s++) {
-        for (int m = 0; m < 3; m++) {
-            for(int i = 0; i < L; i++) {
-                h_vdata[L*(2*(3*s + m) + 0) + i] = m;
-                h_vdata[L*(2*(3*s + m) + 1) + i] = m;
-            }
-        }
-    }
-    
-    // allocate and load device memory
-    float* d_vdata; // input vectors
-    float* d_mdata; // matrices
-    float* d_odata; // output vectors
-    int* d_index;
-    CUDA_SAFE_CALL(cudaMalloc((void**) &d_vdata, L*SPINOR_BYTES));
-    CUDA_SAFE_CALL(cudaMalloc((void**) &d_mdata, L*GAUGE_BYTES));
-    CUDA_SAFE_CALL(cudaMalloc((void**) &d_odata, L*SPINOR_BYTES));
-    CUDA_SAFE_CALL(cudaMalloc((void**) &d_index, 8*L*sizeof(int)));
-    CUDA_SAFE_CALL(cudaMemcpy(d_vdata, h_vdata, L*SPINOR_BYTES, cudaMemcpyHostToDevice));
-    CUDA_SAFE_CALL(cudaMemcpy(d_mdata, h_mdata, L*GAUGE_BYTES, cudaMemcpyHostToDevice));
-    CUDA_SAFE_CALL(cudaMemcpy(d_mdata, h_mdata, L*GAUGE_BYTES, cudaMemcpyHostToDevice));
-    CUDA_SAFE_CALL(cudaMemcpy(d_index, h_index, 8*L*sizeof(int), cudaMemcpyHostToDevice));
-    
-    cudaBindTexture(0 /*offset*/, spinor, d_vdata, L*SPINOR_BYTES); 
-    cudaBindTexture(0 /*offset*/, gauge, d_mdata, L*GAUGE_BYTES); 
+    // construct input fields
+    float* gaugeIn = (float *)malloc(L*GAUGE_BYTES);
+    float* spinorIn = (float *)malloc(L*SPINOR_BYTES);
+    float* spinorOut = (float*)malloc(L*SPINOR_BYTES);
+    float* spinorRef = (float*)malloc(L*SPINOR_BYTES);
 
+    // allocate host memory
+    float4* packedGaugeIn = (float4*) malloc(L*PACKED_GAUGE_BYTES);
+    float4* packedSpinorIn = (float4*) malloc(L*SPINOR_BYTES);
+    float4* packedSpinorOut = (float4*) malloc(L*SPINOR_BYTES);
+    
+    // allocate device memory
+    float4* d_gaugeIn;
+    float4* d_spinorIn;
+    float4* d_spinorOut;
+    CUDA_SAFE_CALL(cudaMalloc((void**) &d_gaugeIn, L*PACKED_GAUGE_BYTES));
+    CUDA_SAFE_CALL(cudaMalloc((void**) &d_spinorIn, L*SPINOR_BYTES));
+    CUDA_SAFE_CALL(cudaMalloc((void**) &d_spinorOut, L*SPINOR_BYTES));
+    
+    // copy inputs from host to device
+    constructGaugeField(gaugeIn);
+    constructSpinorField(spinorIn);
+    packGaugeField(packedGaugeIn, gaugeIn);
+    packSpinorField(packedSpinorIn, spinorIn);
+
+    CUDA_SAFE_CALL(cudaMemcpy(d_gaugeIn, packedGaugeIn, L*PACKED_GAUGE_BYTES, cudaMemcpyHostToDevice));
+    CUDA_SAFE_CALL(cudaMemcpy(d_spinorIn, packedSpinorIn, L*SPINOR_BYTES, cudaMemcpyHostToDevice));
+    
+    // bind textures
+    cudaBindTexture(0 /*offset*/, gaugeTex, d_gaugeIn, L*PACKED_GAUGE_BYTES); 
+    cudaBindTexture(0 /*offset*/, spinorTex, d_spinorIn, L*SPINOR_BYTES); 
+    
     // execute kernel
     const int LOOPS = 100;
     dim3 gridDim(GRID_DIM, 1, 1);
     dim3 blockDim(BLOCK_DIM, 1, 1);
     cutStartTimer(timer);
     for (int i = 0; i < LOOPS; i++) {
-        testKernel <<<gridDim, blockDim, shared_bytes>>> ((float4 *)d_odata, d_index);
+        testKernel <<<gridDim, blockDim, shared_bytes>>> ((float4 *)d_spinorOut);
         CUT_CHECK_ERROR("Kernel execution failed");
         cudaThreadSynchronize();
     }
@@ -102,43 +81,30 @@ runTest( int argc, char** argv)  {
     float secs = millisecs / 1000.;
     printf("Elapsed time %fms\n", millisecs);
     printf("GFLOPS = %f\n", 1e-9*1320*L/secs);
-    printf("GiB = %f\n", 2*(1*7+4)*3*4*L/(secs*(1<<30)));
+    printf("GiB = %f\n", 2*(8*7+4)*3*4*L/(secs*(1<<30)));
     
     // compare to host computation
-    CUDA_SAFE_CALL(cudaMemcpy(h_odata, d_odata, L*SPINOR_BYTES, cudaMemcpyDeviceToHost));
-    
-/*
-    cutResetTimer(timer);
-    cutStartTimer(timer);
-    for (int i = 0; i < 100; i++) {
-        computeGold(reference, h_vdata, h_mdata, NUM_MATS);
-    }
-    cutStopTimer(timer);
-    printf( "Host processing time: %f (ms)\n", cutGetTimerValue( timer));
-*/
-
-/*
-    printf("comparing %f kbytes\n", (L*SPINOR_BYTES)/1024.);
-    CUTBoolean res = cutComparefe(reference, h_odata, L*SPINOR_BYTES, 1e-6);
+    CUDA_SAFE_CALL(cudaMemcpy(packedSpinorOut, d_spinorOut, L*SPINOR_BYTES, cudaMemcpyDeviceToHost));
+    unpackSpinorField(spinorOut, packedSpinorOut);
+    computeGold(spinorRef, gaugeIn, spinorIn);
+    CUTBoolean res = cutComparefe(spinorOut, spinorRef, L*SPINOR_BYTES, 1e-6);
     printf("Test %s\n", (1 == res) ? "PASSED" : "FAILED");
-*/
-
-
-    for (int i = 0; i < 10; i++) {
-//        printVector(i, &reference[i]);
-        printVector(i, &h_odata[i]);
-    }
-
+    
+    printSpinorField(spinorRef);
+    printf("\n");
+    printSpinorField(spinorOut);
+    
     // cleanup memory
-    free(h_mdata);
-    free(h_vdata);
-    free(h_odata);
-    free(h_index);
-    free(reference);
-    CUDA_SAFE_CALL(cudaFree(d_mdata));
-    CUDA_SAFE_CALL(cudaFree(d_vdata));
-    CUDA_SAFE_CALL(cudaFree(d_odata));
-    CUDA_SAFE_CALL(cudaFree(d_index));
+    free(gaugeIn);
+    free(spinorIn);
+    free(spinorOut);
+    free(spinorRef);
+    free(packedGaugeIn);
+    free(packedSpinorIn);
+    free(packedSpinorOut);
+    CUDA_SAFE_CALL(cudaFree(d_gaugeIn));
+    CUDA_SAFE_CALL(cudaFree(d_spinorIn));
+    CUDA_SAFE_CALL(cudaFree(d_spinorOut));
     cutDeleteTimer(timer);
 }
 
--- trunk/kip/Cuda/dslash/qcd_gold.cpp
+++ trunk/kip/Cuda/dslash/qcd_gold.cpp
@@ -1,33 +1,173 @@
+
+typedef struct {
+    float x, y, z, w;
+} float4;
+
+#include <stdio.h>
+#include <stdlib.h>
 #include "qcd.h"
 
-extern "C" 
-void computeGold(float* reference, float *vecs, float* mats, const int len);
 
-/*
-void
-dot(float* a, float* b, float& re, float &im) {
-    re = im = 0;
-    for (int i = 0; i < dim; i++) {
-        float a_re = a[2*i+0];
-        float a_im = a[2*i+1];
-        float b_re = b[2*i+0];
-        float b_im = b[2*i+1];
-        re += a_re * b_re - a_im * b_im;
-        im += a_re * b_im + a_im * b_re;
+int index(int z, int y, int x, int t) {
+    z = (z + L3) % L3;
+    y = (y + L2) % L2;
+    x = (x + L1) % L1;
+    t = (t + L0) % L0;
+    return z*L2*L1*L0 + y*L1*L0 + x*L0 + t;
+}
+
+int coord_0(int idx) {
+    return idx % L0;
+}
+
+int coord_1(int idx) {
+    return (idx/L0) % L1;
+}
+
+int coord_2(int idx) {
+    return (idx/(L0*L1)) % L2;
+}
+
+int coord_3(int idx) {
+    return idx/(L0*L1*L2);
+}
+
+int nextIndex(int i, int dir) {
+    int t = coord_0(i);
+    int x = coord_1(i);
+    int y = coord_2(i);
+    int z = coord_3(i);
+    switch (dir) {
+        case 0: return index(z, y, x, t+1);
+        case 1: return index(z, y, x+1, t-1);
+        case 2: return index(z, y, x-2, t);
+        case 3: return index(z, y+1, x+1, t);
+        case 4: return index(z, y-2, x, t);
+        case 5: return index(z+1, y+1, x, t);
+        case 6: return index(z-2, y, x, t);
+        default: return -1;
     }
 }
 
-void
-computeGold(float* reference, float *vecs, float* mats, const int len) {
-    // loop over vectors
-    for (int i = 0; i < len; i++) {
-        // loop over elements of vector
-        for (int j = 0; j < dim; j++) {
-            float re = 0, im = 0;
-            dot(&mats[i*matsize + j*vecsize], &vecs[i*vecsize], re, im);
-            reference[i*vecsize + 2*j + 0] = re;
-            reference[i*vecsize + 2*j + 1] = im;
+void packGaugeField(float4 *res, float *gauge) {
+    for (int i = 0; i < L; i++) {
+        for (int dir = 0; dir < 4; dir++) {
+            for (int j = 0; j < 5; j++) {
+                float a1, a2, a3, a4;
+                a1 = gauge[i*4*18 + dir*18 + 4*j + 0];
+                a2 = gauge[i*4*18 + dir*18 + 4*j + 1];
+                if (j < 4) {
+                    a3 = gauge[i*4*18 + dir*18 + 4*j + 2];
+                    a4 = gauge[i*4*18 + dir*18 + 4*j + 3];
+                }
+                else {
+                    int i3 = nextIndex(i, 2*dir+0);
+                    int i4 = nextIndex(i, 2*dir+1);
+                    a3 = *(float *)&i3;
+                    a4 = *(float *)&i4;
+                }
+                float4 f4 = {a1, a2, a3, a4};
+                res[(dir*5+j)*L + i] = f4;
+            }
         }
     }
 }
-*/
+
+void packSpinorField(float4 *res, float *spinor) {
+    for (int i = 0; i < L; i++) {
+        for (int j = 0; j < 6; j++) {
+            float a1 = spinor[i*24 + 4*j + 0];
+            float a2 = spinor[i*24 + 4*j + 1];
+            float a3 = spinor[i*24 + 4*j + 2];
+            float a4 = spinor[i*24 + 4*j + 3];
+            float4 f4 = {a1, a2, a3, a4};
+            res[j*L + i] = f4;
+        }
+    }
+}
+
+void unpackSpinorField(float *res, float4 *spinorPacked) {
+    for (int i = 0; i < L; i++) {
+        for (int j = 0; j < 6; j++) {
+            float4 f4 = spinorPacked[j*L + i];
+            res[i*24 + 4*j + 0] = f4.x;
+            res[i*24 + 4*j + 1] = f4.y;
+            res[i*24 + 4*j + 2] = f4.z;
+            res[i*24 + 4*j + 3] = f4.w;
+        }
+    }
+}
+
+
+void constructGaugeField(float *res) {
+    for(int i = 0; i < L; i++) {
+        for (int dir = 0; dir < 4; dir++) {
+            for (int m = 0; m < 3; m++) {
+                for (int n = 0; n < 3; n++) {
+                    res[i*(4*3*3*2) + dir*(3*3*2) + m*(3*2) + n*(2) + 0] = 1;
+                    res[i*(4*3*3*2) + dir*(3*3*2) + m*(3*2) + n*(2) + 1] = 0;
+                }
+            }
+        }
+    }
+}
+
+
+void constructSpinorField(float *res) {
+    for(int i = 0; i < L; i++) {
+        for (int s = 0; s < 4; s++) {
+            for (int m = 0; m < 3; m++) {
+                res[i*(4*3*2) + s*(3*2) + m*(2) + 0] = m;
+                res[i*(4*3*2) + s*(3*2) + m*(2) + 1] = 0;
+            }
+        }
+    }
+}
+
+
+void dot(float* res, float* a, float* b) {
+    res[0] = res[1] = 0;
+    for (int m = 0; m < 3; m++) {
+        float a_re = a[2*m+0];
+        float a_im = a[2*m+1];
+        float b_re = b[2*m+0];
+        float b_im = b[2*m+1];
+        res[0] += a_re * b_re - a_im * b_im;
+        res[1] += a_re * b_im + a_im * b_re;
+    }
+}
+
+void su3_mul(float *res, float *mat, float *vec) {
+    for (int n = 0; n < 3; n++) {
+        dot(&res[n*(2)], &mat[n*(3*2)], vec);
+    }
+}
+
+void multiplyGaugeBySpinor(float *res, int dir, float *gauge, float *spinor) {
+    for (int s = 0; s < 4; s++) {
+        su3_mul(&res[s*(3*2)], gauge, &spinor[s*(3*2)]);
+    }
+}
+
+void computeGold(float* res, float* gauge, float *spinor) {
+    for (int i = 0; i < L; i++) {
+        multiplyGaugeBySpinor(&res[i*(4*3*2)], 0, &gauge[i*(4*3*3*2)], &spinor[i*(4*3*2)]);
+    }
+}
+
+
+void printVector(float *v) {
+    printf("{(%f %f) (%f %f) (%f %f)}\n", v[0], v[1], v[2], v[3], v[4], v[5]);
+}
+
+
+void printSpinorField(float *spinor) {
+    for (int s = 0; s < 4; s++) {
+        printVector(&spinor[s*(3*2)]);
+    }
+    printf("...\n");
+    for (int s = 0; s < 4; s++) {
+        printVector(&spinor[(L-1)*(4*3*2) + s*(3*2)]);
+    }
+    printf("\n");    
+}
