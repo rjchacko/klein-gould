bbbc6d002778147a15282f0f76952141a60fd6b5
kbarros <kbarros@9a1f5bf3-a548-0410-9e96-a52f46714620>
2008-06-05 00:23:06 +0000

    Possibly correct even/odd dslash, including difficult indexing.

--- kip/Cuda/dslash/qcd.h
+++ kip/Cuda/dslash/qcd.h
@@ -1,9 +1,11 @@
 
-#define L1 8 // "x" dimension
+#define L1 16 // "x" dimension
 #define L2 16 // "y" dimension
 #define L3 16 // "z" dimension
 #define L4 64 // "time" dimension
-#define L (L1*L2*L3*L4) // total number of lattice points
+
+#define L1h (L1/2) // half of the full "x" dimension, useful for even/odd lattices
+#define L (L1h*L2*L3*L4) // total number of even/odd lattice points
 
 #define BLOCK_DIM (64) // threads per block
 #define GRID_DIM (L/BLOCK_DIM) // there are L threads in total
@@ -12,7 +14,7 @@
 
 extern "C" void constructGaugeField(float **resEven, float **resOdd);
 extern "C" void constructSpinorField(float *res);
-extern "C" void dslashReference(float *res, float **gaugeEven, float **gaugeOdd, float *spinor, int evenBit);
+extern "C" void dslashReference(float *res, float **gaugeEven, float **gaugeOdd, float *spinorField, int oddBit);
 extern "C" void printSpinorField(float *spinor);
 
 //---------- dslash_cuda.cu
@@ -27,4 +29,4 @@ extern "C" void retrieveSpinorFieldOdd(float *res);
 extern "C" void initializeCuda(int argc, char** argv);
 extern "C" void releaseCuda();
 
-extern "C" void dslashCuda(int evenBit);
+extern "C" void dslashCuda(int oddBit);
