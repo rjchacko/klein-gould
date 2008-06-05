4650b146e475c11cbd2cdd1942828f5795f4624a
kbarros <kbarros@9a1f5bf3-a548-0410-9e96-a52f46714620>
2008-06-05 04:28:54 +0000

    Added dslash_test file, better spinor printing.

--- trunk/kip/Cuda/dslash/qcd.h
+++ trunk/kip/Cuda/dslash/qcd.h
@@ -4,20 +4,24 @@
 #define L3 16 // "z" dimension
 #define L4 64 // "time" dimension
 
-#define L1h (L1/2) // half of the full "x" dimension, useful for even/odd lattices
-#define L (L1h*L2*L3*L4) // total number of even/odd lattice points
+#define L1h (L1/2) // half of the full "x" dimension, useful for even/odd lattice indexing
+#define L (L1h*L2*L3*L4) // poorly named; total number of even/odd lattice points
 
-#define BLOCK_DIM (64) // threads per block
-#define GRID_DIM (L/BLOCK_DIM) // there are L threads in total
 
-//---------- dslash_reference.cpp
+// ---------- dslash_reference.cpp ----------
 
+extern "C" void constructUnitGaugeField(float **resEven, float **resOdd);
 extern "C" void constructGaugeField(float **resEven, float **resOdd);
+extern "C" void constructPointSpinorField(float *resEven, float *resOdd, int i0, int s0, int c0);
 extern "C" void constructSpinorField(float *res);
-extern "C" void dslashReference(float *res, float **gaugeEven, float **gaugeOdd, float *spinorField, int oddBit);
-extern "C" void printSpinorField(float *spinor);
 
-//---------- dslash_cuda.cu
+extern "C" void dslashReference(float *res, float **gaugeEven, float **gaugeOdd, float *spinorField, int oddBit);
+
+extern "C" void printSpinorElement(float *spinorEven, float *spinorOdd, int X);
+extern "C" void printSpinorHalfField(float *spinor);
+
+
+// ---------- dslash_cuda.cu ----------
 
 extern "C" void sendGaugeField(float **gaugeEven, float **gaugeOdd);
 extern "C" void sendSpinorFieldEven(float *spinorEven);
