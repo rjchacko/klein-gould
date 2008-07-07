9dbfd263b87d064db000b6744d9d44a33b400382
kbarros <kbarros@9a1f5bf3-a548-0410-9e96-a52f46714620>
2008-07-07 20:20:50 +0000

    Update memtest.cu

--- trunk/kip/Cuda/memtest/memtest.cu
+++ trunk/kip/Cuda/memtest/memtest.cu
@@ -1,42 +1,57 @@
 
 #include <stdio.h>
-
 #include <cutil.h>
 
 
-#define L (1<<14)
-#define BLOCK_DIM 64
-#define GRID_DIM (L/BLOCK_DIM)
-#define ROWS 200
-#define ARRAY_BYTES (L*ROWS*8*sizeof(float))
+#define SHARED_BYTES (0)
 
+#define ARRAY_BYTES (1<<29)
+#define ARRAY_FLOATS (ARRAY_BYTES / sizeof(float))
+#define BLOCK_DIM 128
+#define GRID_DIM 128
+
+#define THREADS (BLOCK_DIM*GRID_DIM)
+#define LOOPS (ARRAY_FLOATS/(8*THREADS))
 
 
 texture<float2, 1, cudaReadModeElementType> tex2;
 texture<float4, 1, cudaReadModeElementType> tex4;
 
 extern __shared__ float s_data1[];
-extern __shared__ float2 s_data[];
+extern __shared__ float2 s_data2[];
 extern __shared__ float4 s_data4[];
 
-__global__ void
-testKernel1(float *data) {
+
+__global__ void testKernel1(float *data) {
     const int bid = blockIdx.x;
     const int tid = threadIdx.x;
     const int sid = BLOCK_DIM*bid + tid;
     int sp_idx = sid;
     
-    for (int i = 0; i < ROWS; i++) {
-        s_data1[tid] = data[sp_idx]; sp_idx += L;
-        s_data1[tid] = data[sp_idx]; sp_idx += L;
-        s_data1[tid] = data[sp_idx]; sp_idx += L;
-        s_data1[tid] = data[sp_idx]; sp_idx += L;
-        s_data1[tid] = data[sp_idx]; sp_idx += L;
-        s_data1[tid] = data[sp_idx]; sp_idx += L;
-        s_data1[tid] = data[sp_idx]; sp_idx += L;
-        s_data1[tid] = data[sp_idx]; sp_idx += L;
+    float acc = 0;
+    for (int i = 0; i < ARRAY_FLOATS/(2*THREADS); i++) {
+        acc += data[sp_idx]; sp_idx += THREADS;
+        acc += data[sp_idx]; sp_idx += THREADS;
     }
+    s_data1[tid] = acc;
 }
+
+
+__global__ void testKernel1b (float *data) {
+    unsigned int tid = threadIdx.x;
+    unsigned int bid = blockIdx.x;
+    unsigned int gridSize = GRID_DIM*(2*BLOCK_DIM);
+    unsigned int i = bid*(2*BLOCK_DIM) + tid;
+    
+    float acc = 0;
+    while (i < ARRAY_FLOATS) {
+        acc += data[i] + data[BLOCK_DIM+i];
+        i += gridSize;
+    }
+    
+    s_data1[tid] = acc;
+}
+
 
 __global__ void
 testKernel2(float2 *data) {
@@ -45,11 +60,11 @@ testKernel2(float2 *data) {
     const int sid = BLOCK_DIM*bid + tid;
     int sp_idx = sid;
     
-    for (int i = 0; i < ROWS; i++) {
-        s_data[tid] = data[sp_idx]; sp_idx += L;
-        s_data[tid] = data[sp_idx]; sp_idx += L;
-        s_data[tid] = data[sp_idx]; sp_idx += L;
-        s_data[tid] = data[sp_idx]; sp_idx += L;
+    for (int i = 0; i < LOOPS; i++) {
+        s_data2[tid] = data[sp_idx]; sp_idx += THREADS;
+        s_data2[tid] = data[sp_idx]; sp_idx += THREADS;
+        s_data2[tid] = data[sp_idx]; sp_idx += THREADS;
+        s_data2[tid] = data[sp_idx]; sp_idx += THREADS;
     }
 }
 
@@ -60,11 +75,11 @@ testKernel2T() {
     const int sid = BLOCK_DIM*bid + tid;
     int sp_idx = sid;
     
-    for (int i = 0; i < ROWS; i++) {
-        s_data[tid] = tex1Dfetch(tex2, sp_idx+=L);
-        s_data[tid] = tex1Dfetch(tex2, sp_idx+=L);
-        s_data[tid] = tex1Dfetch(tex2, sp_idx+=L);
-        s_data[tid] = tex1Dfetch(tex2, sp_idx+=L);
+    for (int i = 0; i < LOOPS; i++) {
+        s_data2[tid] = tex1Dfetch(tex2, sp_idx+=THREADS);
+        s_data2[tid] = tex1Dfetch(tex2, sp_idx+=THREADS);
+        s_data2[tid] = tex1Dfetch(tex2, sp_idx+=THREADS);
+        s_data2[tid] = tex1Dfetch(tex2, sp_idx+=THREADS);
     }
 }
 
@@ -75,30 +90,31 @@ testKernel4T() {
     const int sid = BLOCK_DIM*bid + tid;
     int sp_idx = sid;
     
-    for (int i = 0; i < ROWS/2; i++) {
-        s_data4[tid] = tex1Dfetch(tex4, sp_idx+=L);
-        s_data4[tid] = tex1Dfetch(tex4, sp_idx+=L);
-        s_data4[tid] = tex1Dfetch(tex4, sp_idx+=L);
-        s_data4[tid] = tex1Dfetch(tex4, sp_idx+=L);
+    for (int i = 0; i < LOOPS/2; i++) {
+        s_data4[tid] = tex1Dfetch(tex4, sp_idx+=THREADS);
+        s_data4[tid] = tex1Dfetch(tex4, sp_idx+=THREADS);
+        s_data4[tid] = tex1Dfetch(tex4, sp_idx+=THREADS);
+        s_data4[tid] = tex1Dfetch(tex4, sp_idx+=THREADS);
     }
 }
 
 
-int main() {
+int main(int argc, char** argv) {
     int nIters = 100;
-    int sharedBytes = 4500;
-    
-    CUDA_SAFE_CALL( cudaSetDevice(1) );
+    double MiB = (double)ARRAY_BYTES / (1<<20);
+    double GiB = (double)ARRAY_BYTES / (1<<30);
 
-    float2 *d_idata;
-    CUDA_SAFE_CALL( cudaMalloc((void**)&d_idata, ARRAY_BYTES) );
+    CUT_DEVICE_INIT(argc, argv);
+    // CUDA_SAFE_CALL( cudaSetDevice(1) );
+
+    void *d_idata;
+    CUDA_SAFE_CALL( cudaMalloc(&d_idata, ARRAY_BYTES) );
     CUDA_SAFE_CALL( cudaBindTexture(0 /*offset*/, tex2, d_idata, ARRAY_BYTES) ); 
-    CUDA_SAFE_CALL( cudaBindTexture(0 /*offset*/, tex4, (float4*)d_idata, ARRAY_BYTES) ); 
+    CUDA_SAFE_CALL( cudaBindTexture(0 /*offset*/, tex4, d_idata, ARRAY_BYTES) ); 
 
-    printf("Global kb: %f\n", ARRAY_BYTES/1024.);
-    printf("Shared kb: %fkB\n", sharedBytes/1024.);
-
-
+    printf("Global MiB: %f\n", MiB);
+    printf("Shared kb: %fkB\n", SHARED_BYTES/1024.);
+    
     dim3 threads(BLOCK_DIM, 1, 1);
     dim3 grid(GRID_DIM, 1, 1);
     
@@ -107,22 +123,26 @@ int main() {
     CUDA_SAFE_CALL( cudaEventCreate(&end) );
     CUDA_SAFE_CALL( cudaEventRecord(start, 0) );
     for (int i=0; i < nIters; ++i) {
-        switch (-1) {
+        switch (-2) {
+        case -2:
+            if (i == 0) printf("Reduction float\n");
+            testKernel1b<<< grid, threads, SHARED_BYTES >>>((float*)d_idata);
+            break;
         case -1:
             if (i == 0) printf("Straight float\n");
-            testKernel1<<< grid, threads, sharedBytes >>>((float*)d_idata);
+            testKernel1<<< grid, threads, SHARED_BYTES >>>((float*)d_idata);
             break;
         case 0:
             if (i == 0) printf("Straight float2\n");
-            testKernel2<<< grid, threads, sharedBytes >>>(d_idata);
+            testKernel2<<< grid, threads, SHARED_BYTES >>>((float2*)d_idata);
             break;
         case 1:
             if (i == 0) printf("Texture float2\n");
-            testKernel2T<<< grid, threads, sharedBytes >>>();
+            testKernel2T<<< grid, threads, SHARED_BYTES >>>();
             break;
         case 2:
             if (i == 0) printf("Texture float4\n");
-            testKernel4T<<< grid, threads, sharedBytes >>>();
+            testKernel4T<<< grid, threads, SHARED_BYTES >>>();
             break;
         }
     }
@@ -131,7 +151,8 @@ int main() {
 
     float runTime;
     CUDA_SAFE_CALL( cudaEventElapsedTime(&runTime, start, end) );
-    runTime /= float(nIters);
+    double secs = runTime / 1000.;
+    
     printf("Average time: %f ms\n", runTime);
-    printf("Bandwidth:    %f GiB/s\n\n", ARRAY_BYTES / (runTime * 1.0e-3 * 1024*1024*1024));
+    printf("Bandwidth:    %f GiB/s\n\n", GiB*nIters / secs);
 }
