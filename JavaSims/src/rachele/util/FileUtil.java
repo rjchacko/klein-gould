9532867b541cb8e79eaee083eff1a7e31b58925c
erg.dominguez <erg.dominguez@9a1f5bf3-a548-0410-9e96-a52f46714620>
2008-09-01 14:28:27 +0000

    changes to svk for DO

--- JavaSims/src/rachele/util/FileUtil.java
+++ JavaSims/src/rachele/util/FileUtil.java
@@ -85,6 +85,16 @@ public class FileUtil {
 		}
 	}
 
+	static public void printlnToFile(String fileName, double d1, double d2, double d3, double d4, double d5){
+		try{
+			File file = new File(fileName);
+			PrintWriter pw = new PrintWriter(new FileWriter(file, true), true);
+			pw.println(d1 + " " + d2 + " " + d3 + " " + d4 + " " + d5);
+		} catch (IOException ex){
+			ex.printStackTrace();
+		}
+	}
+	
 	static public void printAccumToFile(String fileName, Accumulator acc){
 		DatasetBuffer data = acc.copyData();
 		int size = data.size();
