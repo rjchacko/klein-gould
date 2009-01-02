533c08287f5b270d3f819ea43b064290a8d823a9
wakkadojo <wakkadojo@9a1f5bf3-a548-0410-9e96-a52f46714620>
2009-01-02 04:28:11 +0000

    error checking tc trials implemented.

--- trunk/kip/jake/cuda/ising/main.cpp
+++ trunk/kip/jake/cuda/ising/main.cpp
@@ -32,18 +32,18 @@ double abs_var (double * arr, int length)
 void find_tc ()
 {
     // Stauffer Tc ~ 12.869019123
-    int l = 8;
-    int d = 7;
-    double h = 0.00;
-    double Tmin = 11.0;
-    double Tmax = 14.0;
-    double dT = 0.005;
-    //int l = 256;
-    //int d = 2;
+    //int l = 8;
+    //int d = 7;
     //double h = 0.00;
-    //double Tmin = 2.2;
-    //double Tmax = 2.4;
-    //double dT = 0.002;
+    //double Tmin = 11.0;
+    //double Tmax = 14.0;
+    //double dT = 0.005;
+    int l = 512;
+    int d = 2;
+    double h = 0.001;
+    double Tmin = 2.2;
+    double Tmax = 2.4;
+    double dT = 0.001;
 
     int relaxTime = 1000;
     int iters = 4000;
@@ -80,17 +80,121 @@ void find_tc ()
     delete M; delete E; fclose (out);
 }
 
+void find_tc2 ()
+{
+    // // Stauffer Tc ~ 12.869019123
+    //int l = 4;
+    //int d = 7;
+    //double h = 0.00;
+    //double Tmin = 11.0;
+    //double Tmax = 14.0;
+    //double dT = 0.01;
+    int l = 64;
+    int d = 2;
+    double h = 0.00;
+    double Tmin = 2.2;
+    double Tmax = 2.4;
+    double dT = 0.005;
+
+    int relaxTime = 1000;
+    int iters = 1000;
+
+    double t_m = 0.0;
+    double t_e = 0.0;
+    double sum_m = 0.0;
+    double sum_e = 0.0;
+    double sum_m2 = 0.0;
+    double sum_e2 = 0.0;
+    double t_chi = 0.0;
+    double t_cv = 0.0;
+    double chi = 0.0;
+    double cv = 0.0;
+    double chi2 = 0.0;
+    double cv2 = 0.0;
+    double sum_chi = 0.0;
+    double sum_cv = 0.0;
+    double sum_chi2 = 0.0;
+    double sum_cv2 = 0.0;
+    double sig_cv_err;
+    double sig_chi_err;
+
+    double err_lim = 0.1;
+    int min_samples = 15;
+    int max_samples = 150;
+    int nsamples = 0;
+
+    FILE * out = NULL; 
+    out = fopen ("data/tc.dat", "w");
+    if (out == NULL)
+        printf ("Error opening file.\n");
+
+    for (double T=Tmin; T<=Tmax; T+=dT)
+    {
+        IsingCuda * ic = new IsingCuda (l, d, h, T);
+        //Ising1 * ic = new Ising1 (l, d, h, T);
+        //Ising2 * ic = new Ising2 (l, d, h, T);
+        ic->allSpinsUp ();
+
+        sum_chi = sum_cv = sum_chi2 = sum_cv2 = 0.0;
+        nsamples = 0;
+        for (int i=0; i<relaxTime; ++i)
+            ic->update ();
+        do
+        {
+            sum_m = sum_e = sum_m2 = sum_e2 = 0.0;
+            for (int i=0; i<iters; ++i)
+            {
+                ic->update ();
+                t_m = fabs (ic->magnetization ()) / ic->n;
+                t_e = ic->energy () / ic->n;
+                sum_m += t_m; sum_e += t_e;
+                t_m *= t_m; t_e *= t_e;
+                sum_m2 += t_m; sum_e2 += t_e;
+            }
+            sum_m /= iters; sum_e /= iters;
+            sum_m2 /= iters; sum_e2 /= iters;
+
+            ++nsamples;
+            
+            t_chi = (sum_m2 - sum_m*sum_m)/ic->T;
+            t_cv = (sum_e2 - sum_e*sum_e)/ic->T/ic->T;
+            sum_chi += t_chi; sum_cv += t_cv;
+            t_chi *= t_chi; t_cv *= t_cv;
+            sum_chi2 += t_chi; sum_cv2 += t_cv;
+            
+            chi = sum_chi/nsamples; cv = sum_cv/nsamples;
+            chi2 = sum_chi2/nsamples; cv2 = sum_cv2/nsamples;
+
+            sig_chi_err = sqrt ((chi2 - chi*chi)/nsamples);
+            sig_cv_err = sqrt ((cv2 - cv*cv)/nsamples);
+            //printf ("%.4e %.4e\n", chi, sig_chi_err);
+            //printf ("%.4e %d\n", sig_chi/chi, nsamples);
+            if (nsamples > max_samples) break;
+        } while (sig_chi_err/chi > err_lim || nsamples < min_samples);
+
+        if (!(((int) ((T-Tmin)/dT))%10))
+            printf ("finished trial %d/%d\n", (int) ((T-Tmin)/dT), (int)((Tmax-Tmin)/dT));
+        //double chi_n = abs_var (M, dataLen)/T;
+        //double c_n = abs_var (E, dataLen)/T/T;
+        fprintf (out, "%e\t%e\t%e\t%e\t%e\n", T, chi, sig_chi_err, cv, sig_cv_err);
+    }
+    printf ("\n");
+
+    fclose (out);
+}
+
 int main (int argc, char *argv[]) {
     initCuda(argc, argv);
    
-    //test1();
-    //test2();
-    //test3();
+    //test1 ();
+    //test2 ();
+    //test3 ();
     //test4 ();
     //test5 ();
     //test6 ();
     //test7 ();
-    find_tc ();
+    //find_tc ();
+    find_tc2 ();
     
     return 0;
 }
