9a5def42a7f03788ca7884f4b0d06a2a6a2eb457
caserino <caserino@9a1f5bf3-a548-0410-9e96-a52f46714620>
2009-07-29 19:01:00 +0000

    Added a random class which generates random vectors on a d-dimensional hypersphere. Also updated random walk app to use this class, and made changes to TFB app.

--- trunk/JavaSims/src/chris/TFB/TFB.java
+++ trunk/JavaSims/src/chris/TFB/TFB.java
@@ -8,7 +8,7 @@ public class TFB {
 
 	private boolean fibre[];
 	private int Nin, N;
-	private double stress, K, D, beta, phi, ebar[], Dp; //phi = 1 ==> all intact
+	private double stress, K, D, beta, phi, ebar[], fbar[], Dp, Omega, Omega2; //phi = Nalive / N
 	private Random rand;
 
 	public TFB(Parameters params){
@@ -32,6 +32,7 @@ public class TFB {
 		rand   = new Random(seed);
 		fibre  = new boolean[N];
 		ebar   = new double[N];
+		fbar   = new double[N];
 		
 		for(int jj = 0 ; jj < N ; jj++){
 			fibre[jj] = true;
@@ -43,16 +44,14 @@ public class TFB {
 		
 		int s;
 		
-		for (int jj = 0 ; jj < N ; jj++){
+		//for (int jj = 0 ; jj < N ; jj++){
 			s = rand.nextInt(N);
 			if(flip(s)){
 				fibre[s] = !(fibre[s]);
 				Nin += bool2sgn(fibre[s]);
 			}	
 			phi = (double)(Nin) / (double)(N);
-//			// DEBUG
-//			PrintUtil.printlnToFile("/Users/cserino/Desktop/test/debug.txt",phi);
-		}
+		//}
 		
 	}
 	
@@ -63,27 +62,52 @@ public class TFB {
 	
 	private boolean flip(int sj){
 		
-		if(Nin == 1 && fibre[sj]) return false; // dh --> +infinty
+		if(Nin == 1 && fibre[sj]) return false; // dh --> +infinity
 		double dh = (Dp/phi - D)*bool2sgn(fibre[sj]);
 		if (dh <  0) return true;
 		return (rand.nextDouble() < Math.exp(-beta*dh));
 	}
 
-	public double getOmegaInv(double mct){
+	public void calcOmega(double mct){
 		
-		double Omega  = 0;
-		double tmpbar = 0;
+		Omega  = 0;
+		Omega2 = 0;
+		double tmpbar  = 0;
+		double tmpbar2 = 0;
 		
 		for(int jj = 0 ; jj < N ; jj++){
 			ebar[jj] += getHj(fibre[jj]);
 			tmpbar   += ebar[jj];
+			fbar[jj] += (bool2sgn(fibre[jj])+1)/(double)(2);
+			tmpbar2  += fbar[jj];
+//			PrintUtil.printlnToFile("/Users/cserino/desktop/db.txt", getHj(fibre[jj]), (bool2sgn(fibre[jj])+1)/(double)(2));
+			
 		}
-		tmpbar = tmpbar / N;
-		for(int jj = 0 ; jj < N ; jj++){
-			Omega += (ebar[jj]-tmpbar)*(ebar[jj]-tmpbar);
-		}
+		tmpbar  = tmpbar / N;
+		tmpbar2 = tmpbar2 / N;
 		
-		return (mct*mct*N)/Omega;
+//		PrintUtil.printlnToFile("/Users/cserino/desktop/db.txt", "---------------------------------");
+		
+		for(int jj = 0 ; jj < N ; jj++){
+			Omega  += (ebar[jj]-tmpbar)*(ebar[jj]-tmpbar);
+			Omega2 += (fbar[jj]-tmpbar2)*(fbar[jj]-tmpbar2);
+		}
+//		
+//		PrintUtil.printlnToFile("/Users/cserino/desktop/db.txt", Omega, Omega2);
+//		PrintUtil.printlnToFile("/Users/cserino/desktop/db.txt", "___________________________");
+//		
+		Omega  = Omega/(mct*mct*N);
+		Omega2 = Omega2/(mct*mct*N);
+	}
+	
+	public double getOmega1inv(){
+	
+		return 1/Omega;
+	}
+	
+	public double getOmega2inv(){
+		
+		return 1/Omega2;
 	}
 	
 	private double getHj(boolean sj){
